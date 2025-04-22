94b17dc5b689be1d36539dbed115eafb97aaef90
shawonNirob <shawonnirob16@gmail.com>
2025-04-22 21:51:37 +0600

    added a doGet method

--- src/main/java/com/example/servlet_demo/DemoServletNirob.java
+++ src/main/java/com/example/servlet_demo/DemoServletNirob.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/demo-servlet-shown")
+@WebServlet("/demo-servlet-showon")
 public class DemoServletNirob extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException {
